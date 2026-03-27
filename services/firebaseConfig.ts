c0f595ab6975522b795bedea822640ac1b2bc0b5
reagan13 <reaganmabawad@gmail.com>
2026-03-27 19:46:22 +0800

    need to fix the sign in method

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -1,16 +1,17 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIahYXcYWPok9Ijk2lZH3Gh0nDoiU2lUg",
-  authDomain: "fir-7818a.firebaseapp.com",
-  projectId: "fir-7818a",
-  storageBucket: "fir-7818a.firebasestorage.app",
-  messagingSenderId: "323981884251",
-  appId: "1:323981884251:web:c4085f4b40da0555d6f87e",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 export default app;
